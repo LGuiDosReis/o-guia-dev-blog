6b63494b9adea3d9e60cde6482fe566fd00f8de5
Luiz Guilherme Mendonça dos Reis <lgui.dosreis@gmail.com>
2021-12-02 21:07:50 -0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+
+    <nav class="uk-navbar-container" uk-navbar>
+        <div class="uk-navbar-left">
+          <ul class="uk-navbar-nav">
+              <li><a href="#modal-full" uk-toggle><span uk-icon="icon: table"></span></a></li>
+              <li>
+                <a href="/">O guia do desenvolvedor das galáxias
+                </a>
+              </li>
+          </ul>
+        </div>
+
+        <div class="uk-navbar-right uk-visible@s" >
+          <ul class="uk-navbar-nav">
+              <li v-for="category in categories" :key="category.id">
+                <router-link :to="{ name: 'categories-id', params: { id: category.id }}" tag="a">{{ category.name }}
+                </router-link>
+              </li>
+          </ul>
+        </div>
+    </nav>
+
+    <div id="modal-full" class="uk-modal-full" uk-modal>
+        <div class="uk-modal-dialog">
+            <button class="uk-modal-close-full uk-close-large" type="button" uk-close></button>
+            <div class="uk-grid-collapse uk-child-width-1-2@s uk-flex-middle" uk-grid>
+                <span>
+                  <div class="uk-background-cover" style="background-image: url('https://images.unsplash.com/photo-1592561199818-6b69d3d1d6e2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=988&q=80 3308w');" uk-height-viewport></div>
+                </span>
+                <div class="uk-padding-large">
+                    <h1 style="font-family: Staatliches;">O guia do dev</h1>
+                    <div class="uk-width-1-2@s">
+                        <ul class="uk-nav-primary uk-nav-parent-icon" uk-nav>
+                          <li v-for="category in categories" :key="category.id">
+                            <router-link class="uk-modal-close" :to="{ name: 'categories-id', params: { id: category.id }}" tag="a">{{ category.name }}
+                            </router-link>
+                          </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import categoriesQuery from '~/apollo/queries/categories.gql'
+
+export default {
+  data() {
+    return {
+      categories: [],
+    }
+  },
+  apollo: {
+    categories: {
+      prefetch: true,
+      query: categoriesQuery
+    },
+  },
+}
+
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,77 @@
+export default {
+  env: {
+    strapiBaseUri: process.env.API_URL || "http://localhost:1337"
+  },
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'O guia dev',
+    htmlAttrs: {
+      lang: 'pt-br'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Staatliches' }
+    ]
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'uikit/dist/css/uikit.min.css',
+    'uikit/dist/css/uikit.css',
+    '@assets/css/main.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    { src: '~/plugins/uikit.js', ssr: false }
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [  
+    '@nuxtjs/apollo',
+    '@nuxtjs/markdownit',
+    '@nuxtjs/pwa'
+  ],
+  markdownit: {
+    preset: 'default',
+    linkify: true,
+    breaks: true,
+    injected: true
+  },
+  apollo: {  
+    clientConfigs: {
+      default: {
+        httpEndpoint: process.env.BACKEND_URL || "http://localhost:1337/graphql"
+      }
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  },
+  pwa: {
+    meta: {
+      title: 'O guia dev',
+      author: 'Luiz Guilherme Mendonça dos Reis',
+    },
+    manifest: {
+      name: 'O guia do desenvolvedor das galáxias',
+      short_name: 'O guia dev',
+      lang: 'pt-br',
+    },
+  }
+}
